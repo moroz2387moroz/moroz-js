8135a19a57f614b33910e8eeb99e6991ccdec73a
Marina Moroz <moroz2387moroz@yandex.ru>
2026-08-31 17:28:52 +1000

    add: lesson 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Moroz project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 19;
+let fullPrice = 350000;
+let adaptive = false;
 
-alert("天官赐福");
-console.log("镇魂");
+let procent = fullPrice * (rollback / 100);
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани",
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани",
+);
+
+console.log(screens.toLowerCase().split(", "));
+console.log("Процент посреднику: " + procent + " рублей/ долларов/гривен/юани");
